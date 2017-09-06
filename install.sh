9bf3127b9c37b1a3a3513c246c2c0356f796d964
Mike Flynn <mfflynn@gmail.com>
2017-09-06 00:17:19 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Update repo
+# Request config file location.
+# Generate service file.
+# sudo systemctl stop hello.service
+# Copy service file to /lib/systemd/system/...
+# sudo systemctl daemon-reload
+# sudo systemctl enable hello.service
+# sudo systemctl start hello.service
+
+# Uninstall logic...
