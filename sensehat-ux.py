84acc95894031e21971f3572ea30d0b01c5a3dfd
Mike Flynn <mfflynn@gmail.com>
2017-09-05 23:45:30 -0700

    Fully functional. Won't kill the CPU. Still dirty.

--- sensehat-ux.py
+++ sensehat-ux.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import time
 import sys
+import string
 import ConfigParser
+import subprocess
 from sense_hat import SenseHat, ACTION_PRESSED, ACTION_HELD, ACTION_RELEASED
 from sys import argv
 
@@ -14,6 +16,7 @@ blue = Bl = (0, 0, 255)
 black = B = (0, 0, 0)
 white = W = (255, 255, 255)
 green = G = (0, 255, 0)
+orange = O = (255,165,0)
 
 pixel_matrix_logo = [
     R, R, R, R, R, R, R, R,
@@ -27,47 +30,58 @@ pixel_matrix_logo = [
 ]
 
 pixel_matrix_x = [
-        R, R, B, B, B, B, R, R,
-        R, R, R, B, B, R, R, R,
-        B, R, R, R, R, R, R, B,
-        B, B, R, R, R, R, B, B,
-        B, B, R, R, R, R, B, B,
-        B, R, R, R, R, R, R, B,
-        R, R, R, B, B, R, R, R,
-        R, R, B, B, B, B, R, R
+    R, R, B, B, B, B, R, R,
+    R, R, R, B, B, R, R, R,
+    B, R, R, R, R, R, R, B,
+    B, B, R, R, R, R, B, B,
+    B, B, R, R, R, R, B, B,
+    B, R, R, R, R, R, R, B,
+    R, R, R, B, B, R, R, R,
+    R, R, B, B, B, B, R, R
 ]
 
 pixel_matrix_happy = [
-        B, B, B, B, B, B, B, B,
-        B, B, G, B, B, G, B, B,
-        B, B, G, B, B, G, B, B,
-        B, B, B, B, B, B, B, B,
-        B, G, B, B, B, B, G, B,
-        B, B, G, B, B, G, B, B,
-        B, B, B, G, G, B, B, B,
-        B, B, B, B, B, B, B, B
+    B, B, B, B, B, B, B, G,
+    B, B, B, B, B, B, G, G,
+    B, B, B, B, B, G, G, G,
+    B, B, B, B, B, G, G, B,
+    B, G, B, B, G, G, G, B,
+    G, G, G, B, G, G, B, B,
+    B, G, G, G, G, B, B, B,
+    B, B, G, G, G, B, B, B
 ]
 
 pixel_matrix_off = [
-        B, B, B, B, B, B, B, B,
-        B, B, B, B, R, B, B, B,
-        B, B, R, B, R, B, R, B,
-        B, R, B, B, R, B, B, R,
-        B, R, B, B, B, B, B, R,
-        B, B, R, B, B, B, R, B,
-        B, B, B, R, R, R, B, B,
-        B, B, B, B, B, B, B, B
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, R, B, B, B,
+    B, B, R, B, R, B, R, B,
+    B, R, B, B, R, B, B, R,
+    B, R, B, B, B, B, B, R,
+    B, B, R, B, B, B, R, B,
+    B, B, B, R, R, R, B, B,
+    B, B, B, B, B, B, B, B
 ]
 
 pixel_matrix_on = [
-        B, B, B, B, B, B, B, B,
-        B, B, B, B, G, B, B, B,
-        B, B, G, B, G, B, G, B,
-        B, G, B, B, G, B, B, G,
-        B, G, B, B, B, B, B, G,
-        B, B, G, B, B, B, G, B,
-        B, B, B, G, G, G, B, B,
-        B, B, B, B, B, B, B, B
+    B, B, B, B, B, B, B, B,
+    B, B, B, B, G, B, B, B,
+    B, B, G, B, G, B, G, B,
+    B, G, B, B, G, B, B, G,
+    B, G, B, B, B, B, B, G,
+    B, B, G, B, B, B, G, B,
+    B, B, B, G, G, G, B, B,
+    B, B, B, B, B, B, B, B
+]
+
+pixel_matrix_err = [
+    B, B, O, O, O, O, O, B,
+    B, B, O, O, O, O, O, B,
+    B, B, B, B, B, O, O, B,
+    B, B, B, B, O, O, B, B,
+    B, B, B, O, O, B, B, B,
+    B, B, B, O, O, B, B, B,	
+    B, B, B, B, B, B, B, B,
+    B, B, B, O, O, B, B, B
 ]
 
 MODE_WAIT = 1
@@ -119,12 +133,58 @@ def optionDec(options_idx):
 
     return options_idx
 
+def processStatus(idx):
+    try:
+        res = int(subprocess.check_output(options[idx]["status"], shell=True))
+    except subprocess.CalledProcessError:
+        print("Status command failed for {}".format(options[idx]["name"]))
+        res = -1
+    
+    return res
+
+def processStart(idx):
+    try:
+        subprocess.check_call(options[idx]["start"], shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        print("Start failed for {}".format(options[idx]["name"]))
+        return False
+
+def processStop(idx):
+    try:
+        subprocess.check_call(string.split(options[idx]["stop"], " "))
+	return True
+    except subprocess.CalledProcessError:
+        print("Stop failed for {}".format(options[idx]["name"]))
+	return False
+
 def showCurrentOption():
+    status = processStatus(options_idx)
     sense.show_message(options[options_idx]["name"], 0.05)
-    sense.set_pixels(pixel_matrix_off)
+    if status > 0:
+        sense.set_pixels(pixel_matrix_on)
+    elif status == 0:
+        sense.set_pixels(pixel_matrix_off)
+    else:
+        sense.set_pixels(pixel_matrix_err)
 
 def toggleCurrentOption():
-    sense.set_pixels(pixel_matrix_on)
+    status = processStatus(options_idx)
+    if status > 0:
+        res = processStop(options_idx)
+	if res == True:
+            sense.set_pixels(pixel_matrix_off)
+        else:
+            sense.set_pixels(pixel_matrix_err)
+    elif status == 0:
+        if processStart(options_idx) == True:
+            sense.set_pixels(pixel_matrix_on)
+        else:
+	    sense.set_pixels(pixel_matrix_err)
+    else:
+        sense.set_pixels(pixel_matrix_err)
+
+## Main
 
 sense.set_pixels(pixel_matrix_logo)
 time.sleep(1)
@@ -176,3 +236,4 @@ while True:
                 toggleCurrentOption()
 
 	lastEvent = thisEvent
+    time.sleep(0.05)
