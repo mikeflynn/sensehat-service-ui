daa6ae141817ef3ccf6f025d85927f5f885cc0e5
Mike Flynn <mfflynn@gmail.com>
2017-09-06 21:07:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,50 @@
-# Update repo
-# Request config file location.
-# Generate service file.
-# sudo systemctl stop hello.service
-# Copy service file to /lib/systemd/system/...
-# sudo systemctl daemon-reload
-# sudo systemctl enable hello.service
-# sudo systemctl start hello.service
+#!/usr/bin/env bash
 
-# Uninstall logic...
+SERVICE_NAME="sensehat-ui"
+
+if [ "$1" == "" ]; then
+  echo "Missing services.ini path."
+  exit(1)
+fi
+
+if [ "$1" == "--uninstall" ]; then
+  # Uninstall logic...
+  sudo systemctl stop $SERVICE_NAME
+  sudo systemctl disable $SERVICE_NAME
+  sudo rm /lib/systemd/system/$SERVICE_NAME.service 
+  exit(0)
+fi
+
+SERVICE_INI="$1"
+APP_PATH=$(pwd)
+
+# Generate service file.
+SERVICE_FILE=$(cat <<"EOF"
+  [Unit]
+  Description=Sensehat Service UI
+  After=multi-user.target
+
+  [Service]
+  Type=simple
+  ExecStart=__APP_PATH__/sensehat-ux.py -c __CONFIG_PATH__
+  Restart=on-abort
+
+  [Install]
+  WantedBy=multi-user.target
+EOF
+)
+
+SERVICE_FILE="${SERVICE_FILE//__APP_PATH__/$APP_PATH}"
+SERVICE_FILE="${SERVICE_FILE//__CONFIG_PATH__/$SERVICE_INI}"
+
+# Stop current service
+sudo systemctl stop $SERVICE_NAME.service
+
+# Copy service file to /lib/systemd/system/
+sudo echo $SERVICE_FILE > /lib/systemd/system/$SERVICE_NAME.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME.service
+sudo systemctl start $SERVICE_NAME.service
+
+echo "Installation complete."
